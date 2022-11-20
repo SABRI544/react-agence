0343d493cee46f49f5c7268da244b8492c353eb4
sabri <sabri544@msn.com>
2022-11-20 10:29:11 +0100

    pages projet 1/2/3/4

--- src/pages/Project2.js
+++ src/pages/Project2.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+
+import Buttons from "../components/Buttons";
+import Logo from "../components/Logo";
+import Mouse from "../components/Mouse";
+import Navigation from "../components/Navigation";
+import Project from "../components/Project";
 
 const Project2 = () => {
-    return (
-        <div>
-            <h1>project2</h1>
-        </div>
-    );
+  return (
+    <main>
+      <Mouse />
+      <div className="project">
+        <Navigation />
+        <Logo />
+        <Project projectNumber={1} />
+        <Buttons left={"/projet-1"} right={"/projet-3"} />
+      </div>
+    </main>
+  );
 };
 
 export default Project2;
